63bb8e478a5cbd0cff9afad72ac9319b000cf06e
AzconaDavid <azcona.alo.david@salesianospamplona.net>
2026-01-09 09:39:24 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
-      base: './', // <--- AÑADE ESTA LÍNEA AQUÍ
+      base: '/Z-CHESS/', // <--- AÑADE ESTA LÍNEA AQUÍ
       server: {
         port: 3000,
         host: '0.0.0.0',
